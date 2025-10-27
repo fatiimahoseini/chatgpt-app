733edc7457bc23924a400de185d55f6919ba4fd5
fatima <fatiimahoseini@gmail.com>
2025-10-27 21:29:26 +0330

    get question

--- chatbot/views.py
+++ chatbot/views.py
@@ -5,4 +5,5 @@ def home(request):
   # Check for form submission
   if request.method == 'POST':
     question = request.POST.get('question')
+    return render(request, 'home.html', {"question": question})
   return render(request, 'home.html', {})
