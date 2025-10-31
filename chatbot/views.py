e42b680bdbe0d06eede58fdeb7e8920f9cb869d9
fatima <fatiimahoseini@gmail.com>
2025-10-31 16:46:32 +0330

    made request to gemini (sdk didnt work)

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+import requests, json
 
-# Create HomePage
 def home(request):
-  # Check for form submission
-  if request.method == 'POST':
-    question = request.POST.get('question')
-    return render(request, 'home.html', {"question": question})
-  return render(request, 'home.html', {})
+    if request.method == 'POST':
+        question = request.POST.get('question')
+
+        url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
+        headers = {
+            "Content-Type": "application/json",
+            "x-goog-api-key": "AIzaSyC8ayLaWVhKks922ajbT-8cRSJ0qkN48gM"
+        }
+        data = {
+            "contents": [{"parts": [{"text": question}]}]
+        }
+
+        try:
+            r = requests.post(url, headers=headers, data=json.dumps(data))
+            if r.status_code == 200:
+                res = r.json()
+                answer = res["candidates"][0]["content"]["parts"][0]["text"]
+            else:
+                answer = f"Error {r.status_code}: {r.text}"
+        except Exception as e:
+            answer = f"Exception: {e}"
+
+        return render(request, 'home.html', {"question": question, "response": answer})
+
+    return render(request, 'home.html', {})
